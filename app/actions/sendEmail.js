639cc184b44a22b65c90bb5cc42037803d18d464
Ekhlas Uddin Ahmed <matrixedu14@gmail.com>
2025-09-11 23:05:54 +0600

    Email Sending Problem is fixed for second time

--- app/actions/sendEmail.js
+++ app/actions/sendEmail.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
   // service: "gmail", // or use SMTP config
   host: "smtp.gmail.com",
   port: 587,
-  secure:true,
+  secure: false,
   auth: {
     user: `${process.env.EMAIL_USER}`,
     pass: `${process.env.EMAIL_PASS}`,
